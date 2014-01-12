b9c3a3a87eb07eab9c699dcb1476fa15b2ecf278
mattlykins <matt@mattlykins.com>
2014-01-11 21:01:09 -0500

    Add copyright info

--- cgMiner.Monitor/src/com/mattlykins/cgminer/monitor/cgMiner.java
+++ cgMiner.Monitor/src/com/mattlykins/cgminer/monitor/cgMiner.java
@@ -1,3 +1,21 @@
+/*
+Copyright (c) 2014 Matt Lykins
+
+This file is part of Remote Monitor for CGMiner.
+
+Remote Monitor for CGMiner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Remote Monitor for CGMiner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Remote Monitor for CGMiner.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.mattlykins.cgminer.monitor;
 
 
